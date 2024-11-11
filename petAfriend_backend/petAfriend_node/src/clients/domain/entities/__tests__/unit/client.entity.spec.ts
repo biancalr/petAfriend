739477d5fe3909566b1e770b94381923ca1b9832
bianc <bianc@host.docker.internal>
2024-11-11 01:42:08 -0300

    ClientEntity - getter methods unit tests

--- petAfriend_backend/petAfriend_node/src/clients/domain/entities/__tests__/unit/client.entity.spec.ts
+++ petAfriend_backend/petAfriend_node/src/clients/domain/entities/__tests__/unit/client.entity.spec.ts
@@ -17,4 +17,21 @@ describe("ClientEntity", () => {
     expect(sut.props.username).toEqual(props.username);
     expect(sut.props.email).toEqual(props.email);
   });
+
+  it("Getter of username field", () => {
+    expect(sut.props.username).toBeDefined();
+    expect(sut.props.username).toEqual(props.username);
+    expect(typeof sut.props.username).toBe("string");
+  });
+
+  it("Getter of email field", () => {
+    expect(sut.props.email).toBeDefined();
+    expect(sut.props.email).toEqual(props.email);
+    expect(typeof sut.props.email).toBe("string");
+  });
+
+  it("Getter of createdAt field", () => {
+    expect(sut.props.createdAt).toBeDefined();
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
 });
--- petAfriend_backend/petAfriend_node/src/clients/domain/entities/client.entity.ts
+++ petAfriend_backend/petAfriend_node/src/clients/domain/entities/client.entity.ts
@@ -8,4 +8,16 @@ export class ClientEntity {
   constructor(public readonly props: ClientProps) {
     this.props.createdAt = this.props.createdAt ?? new Date();
   }
+
+  get username() {
+    return this.props.username;
+  }
+
+  get email() {
+    return this.props.email;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
 }
