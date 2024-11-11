3e178340476eaba6c55b90955ff450090c3a634b
bianc <bianc@host.docker.internal>
2024-11-11 01:01:59 -0300

    Adding users resources

--- petAfriend_backend/petAfriend_node/src/app.module.ts
+++ petAfriend_backend/petAfriend_node/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { EnvConfigService } from "./shared/infraestructure/env-config/env-config.service";
 import { EnvConfigModule } from "./shared/infraestructure/env-config/env-config.module";
+import { UsersModule } from "./users/infraestructure/users.module";
 
 @Module({
-  imports: [EnvConfigModule],
+  imports: [EnvConfigModule, UsersModule],
   controllers: [AppController],
   providers: [AppService, EnvConfigService],
 })
