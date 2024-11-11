5fe045701c8e2ad00a6cad0a819d97fccaa207b2
bianc <bianc@host.docker.internal>
2024-11-11 00:08:07 -0300

    Refactoring env config module

--- petAfriend_backend/petAfriend_node/src/app.module.ts
+++ petAfriend_backend/petAfriend_node/src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { EnvConfigService } from './shared/infaestructure/env-config/env-config.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { EnvConfigService } from "./shared/infraestructure/env-config/env-config.service";
+import { EnvConfigModule } from "./shared/infraestructure/env-config/env-config.module";
 
 @Module({
-  imports: [],
+  imports: [EnvConfigModule],
   controllers: [AppController],
   providers: [AppService, EnvConfigService],
 })
